75b76de52876246c30827f59cb13183c7cc00463
jongwo0k <pygudo@naver.com>
2025-11-26 23:16:20 +0900

    Feat Implement BossMain logic & StageClear system

--- Assets/Scripts/BossMain.cs
+++ Assets/Scripts/BossMain.cs
@@ -2,16 +2,78 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
-    // HP, UI, ...
+    [Header("Status")]
+    [SerializeField] protected float HP = 100; // 임의
+    protected float currentHP;
     protected bool isDead = false;
+
+    [Header("Component")]
     protected Animator anim;
+    protected Collider col;
+
+    /*
+    [SerializeField] protected Transform player; // 플레이어 위치
+
+    // 코루틴 공통 관리
+    protected Coroutine attackRoutine;
+
+    protected virtual void OnEnable()
+    {
+        attackRoutine = StartCoroutine(AttackRoutine());
+    }
+
+    protected virtual void OnDisable()
+    {
+        if (attackRoutine != null) StopCoroutine(attackRoutine);
+    }
+
+    protected abstract IEnumerator AttackRoutine();
+    */
 
     protected virtual void Start()
     {
+        isDead = false;
+        currentHP = HP;
         anim = GetComponent<Animator>();
+        col = GetComponent<Collider>();
+
+        // UI 초기화
     }
 
-    // TakeDamage()
+    // 피격 처리
+    public virtual void TakeDamage(float damage)
+    {
+        if (isDead) return;
 
-    // Die()
+        currentHP -= damage;
+
+        UpdateUI();
+
+        if (currentHP <= 0)
+        {
+            Die();
+        }
+    }
+
+    // UI 업데이트 (HP, ...)
+    protected virtual void UpdateUI()
+    {
+
+    }
+
+    // 사망 처리
+    protected virtual void Die()
+    {
+        isDead = true;
+        anim.SetBool("isDead", true);
+        col.enabled = false;
+
+        GameManager.Instance.StageClear();
+    }
+
+    // Event에서
+    public void DestroyBoss()
+    {
+        Destroy(gameObject);
+    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,9 +19,38 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    // 게임 시작
     void Start()
     {
         SceneManager.LoadScene("MainMenu");
         Debug.Log("Game Start");
+    }
+
+    // 스테이지 클리어 (보스 처치 성공)
+    // Scene 자동 전환? or 클리어 UI 표시 후 Next 버튼으로 전환
+    public void StageClear()
+    {
+        ClearProjectiles();
+
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentSceneIndex < SceneManager.sceneCountInBuildSettings - 1) // 다음 씬 (Build Settings)
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+        else
+        {
+            // 전체 Game Clear
+        }
+    }
+
+    // 남은 투사체 제거
+    public void ClearProjectiles()
+    {
+        GameObject[] projectiles = GameObject.FindGameObjectsWithTag("Projectile"); // Tag 확인, projectileManager따로? ObjectPooling? Event?
+        foreach (GameObject p in projectiles)
+        {
+            Destroy(p);
+        }
     }
 }
